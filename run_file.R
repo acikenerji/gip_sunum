416a4db7f01b430f613fad970b2997b76228a7cc
Berk Orbay <berkorbay@users.noreply.github.com>
2021-11-26 09:14:11 +0300

    docker trial

--- run_file.R
+++ run_file.R
@@ -2,7 +2,7 @@ library(xaringanBuilder)
 library(intraday)
 library(telegram)
 
-reporting_process <- function(file_name="intraday_presentation.Rmd",the_date=lubridate::today()-1,audio_file_path="background_music/sample1.mp3",telegram_params){
+reporting_process <- function(file_name="intraday_presentation.Rmd",the_date=lubridate::today()-1,audio_file_path="background_music/sample1.mp3",telegram_params=list()){
   
   price_df_raw <- readr::read_csv(system.file("market-data/price_data.csv",package="intraday"),locale=readr::locale(tz="Turkey"))
   
@@ -23,22 +23,22 @@ reporting_process <- function(file_name="intraday_presentation.Rmd",the_date=lub
   
   av::av_encode_video(input=file_vec[order(as.integer(gsub(paste0(".*",base_name,"_|\\.png+$"),"",file_vec)))],audio=audio_file_path,output=paste0(base_name,".mp4"),framerate=0.3,vfilter="scale=trunc(iw/2)*2:trunc(ih/2)*2")
   
-  tbot <- TGBot$new(token = telegram_params$bot_token)
+  # tbot <- TGBot$new(token = telegram_params$bot_token)
   
   print("Sending message to Telegram")
   
   video_duration <- av::av_media_info(paste0(base_name,".mp4"))$duration
   closeAllConnections()
-  print(paste0("Video duration: ",video_duration))
-  for(chat_id in telegram_params$chat_id){
-    print(paste0("Chat ID: ",telegram_params$chat_id))
-    try(tbot$sendMessage("Hello",chat_id = chat_id))
-    
-    try(tbot$sendDocument(normalizePath(paste0(base_name,".pdf")),chat_id = chat_id))
-    try(tbot$sendVideo(normalizePath(paste0(base_name,".mp4")),duration=video_duration,caption=paste0("Gün İçi Piyasası Videosu (",the_date,")"),chat_id = chat_id))
-  }
-  
-  print("End of sending message to Telegram")
+  # print(paste0("Video duration: ",video_duration))
+  # for(chat_id in telegram_params$chat_id){
+  #   print(paste0("Chat ID: ",telegram_params$chat_id))
+  #   try(tbot$sendMessage("Hello",chat_id = chat_id))
+  #   
+  #   try(tbot$sendDocument(normalizePath(paste0(base_name,".pdf")),chat_id = chat_id))
+  #   try(tbot$sendVideo(normalizePath(paste0(base_name,".mp4")),duration=video_duration,caption=paste0("Gün İçi Piyasası Videosu (",the_date,")"),chat_id = chat_id))
+  # }
+  # 
+  # print("End of sending message to Telegram")
   
   
   file.remove(paste0(base_name,".zip"))
@@ -47,31 +47,31 @@ reporting_process <- function(file_name="intraday_presentation.Rmd",the_date=lub
   
 }
 
-send_msg_via_telegram <- function(the_message="Your message here.",api_key="",chat_id="",oops_type=""){
-  telegram_url <- "https://api.telegram.org/bot"
-  full_request <- paste0(telegram_url,api_key,"/sendMessage?chat_id=",chat_id,"&text=",the_message)
-  tryCatch(
-    httr::GET(full_request),
-    error = function(e){
-      print(paste0("OOPS. Could not process Telegram message. Type: ",oops_type," Error is ",e, "Message is: ",the_message))
-    }
-  )
-}
-
-send_document_via_telegram <- function(document_path,api_key="",chat_id="",oops_type=""){
-  telegram_url <- "https://api.telegram.org/bot"
-  the_document <- httr::upload_file(document_path,type="multipart/form-data")
-  full_request <- paste0(telegram_url,api_key,"/sendDocument")
-  the_handle <- handle('')
-  tryCatch(
-    httr::POST(full_request,encode ="multipart",body=list(chat_id=chat_id,document=the_document),handle=the_handle,timeout(60),config(http_version=1.1)),
-    error = function(e){
-      print(paste0("OOPS. Could not process Telegram message. Type: ",oops_type," Error is ",e))
-    }
-  )
-  handle_reset(the_handle$url)
-  rm(the_handle)
-}
-
-
-
+# send_msg_via_telegram <- function(the_message="Your message here.",api_key="",chat_id="",oops_type=""){
+#   telegram_url <- "https://api.telegram.org/bot"
+#   full_request <- paste0(telegram_url,api_key,"/sendMessage?chat_id=",chat_id,"&text=",the_message)
+#   tryCatch(
+#     httr::GET(full_request),
+#     error = function(e){
+#       print(paste0("OOPS. Could not process Telegram message. Type: ",oops_type," Error is ",e, "Message is: ",the_message))
+#     }
+#   )
+# }
+# 
+# send_document_via_telegram <- function(document_path,api_key="",chat_id="",oops_type=""){
+#   telegram_url <- "https://api.telegram.org/bot"
+#   the_document <- httr::upload_file(document_path,type="multipart/form-data")
+#   full_request <- paste0(telegram_url,api_key,"/sendDocument")
+#   the_handle <- handle('')
+#   tryCatch(
+#     httr::POST(full_request,encode ="multipart",body=list(chat_id=chat_id,document=the_document),handle=the_handle,timeout(60),config(http_version=1.1)),
+#     error = function(e){
+#       print(paste0("OOPS. Could not process Telegram message. Type: ",oops_type," Error is ",e))
+#     }
+#   )
+#   handle_reset(the_handle$url)
+#   rm(the_handle)
+# }
+# 
+# 
+# https://github.com/acikenerji/gip_sunum/raw/main/intraday_presentation.pdf
